eff9dac45457cad3ddf76b16a44d11cd8827956b
maw0728 <maw0728@gmail.com>
2023-01-29 01:00:04 +0900

    테이블 추가로직

--- src/lib/main.js
+++ src/lib/main.js
@@ -12,6 +12,54 @@ const getCombinations = (arr, selectNumber)=>{
     return results;
 }
 
+
+
+
+
+
+let submitBtn = document.querySelector('.submit-btn');
+
+submitBtn.addEventListener('click',()=>{
+    let isError = false
+    let errMessage;
+    let numList = [];
+    let result;
+    let numInputList = document.getElementsByClassName('user-num');
+    for (let i = 0; i < 10; i++) {
+        let targetNum = numInputList.item(i).valueAsNumber
+        if (targetNum > 0 && targetNum < 46) {
+            numList.push(targetNum);
+        } else {
+            isError = true;
+            errMessage = '입력이 잘못 되었습니다 다시확인해주세요'
+        }
+    }
+
+    if (!isError) {
+        let resultNum = getCombinations(numList, 6);
+        resultNum.map((x) => console.log(x));
+        for (let i = 0; i < resultNum.length; i++) {
+            document.querySelector("tbody").innerHTML += `<tr></tr>`
+        }
+        resultNum.map((numList,index)=>{
+            document.querySelector("tbody").innerHTML += `<tr>
+              <td class="row-num">${index+1}</td>
+              <td>${numList[0]}</td>
+              <td>${numList[1]}</td>
+              <td>${numList[2]}</td>
+              <td>${numList[3]}</td>
+              <td>${numList[4]}</td>
+              <td>${numList[5]}</td>
+            </tr>`;
+        })
+    } else {
+        return alert(errMessage);
+    }
+});
+
+
+
+
 // 경우의 수 리스트 조회
 // let searchBtn = document.getElementById('searchBtn');
 // searchBtn.addEventListener('click', /*리스트조회함수*/)
@@ -26,4 +74,5 @@ const getCombinations = (arr, selectNumber)=>{
 const testArr = [1,3,4,5,11,13,17,22,37,41]
 const result = getCombinations(testArr, 6);
 console.log(result);
+result.map(x => console.log(x));
 
